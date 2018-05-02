d889d570ffc82fdffce448c65a72c369da4015e6
Samuel <samuel.cruz1@live.com>
2018-05-01 20:38:45 -0600

    commit

---
+++ src/main/java/flyKey/Another.java
@@ -0,0 +1,10 @@
+package flyKey;
+
+import java.io.IOException;
+
+public class Another {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Creating a new network...");
+	}
+}
